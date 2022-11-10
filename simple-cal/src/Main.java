260dcedabeeee8cd0e4da81db5855ee92c5f6441
emirhantash <tasemirhan2546@gmail.com>
2022-11-10 22:26:04 +0300

    java practices
    grade-avg-cal edited
    patika.dev added
    kdv-cal added
    hypo-cal added
    taxi-meter-cal added
    circle-area-perimeter-cal added
    bmi-cal added
    grocery-store added
    simple-cal edited

--- simple-cal/src/Main.java
+++ simple-cal/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int n1,n2,op;
         Scanner input = new Scanner(System.in);
-        System.out.print("enter the first number");
+        System.out.println("enter the first number");
         n1=input.nextInt();
-        System.out.print("enter the second number");
+        System.out.println("enter the second number");
         n2=input.nextInt();
-        System.out.print("enter the operation\n1. addition\t2. extraction\t3. multiplication\t4. division");
+        System.out.println("enter the operation\n1. addition\t2. extraction\t3. multiplication\t4. division");
         op=input.nextInt();
         switch(op) {
             case 1:
@@ -38,6 +38,8 @@ public class Main {
                 System.out.print("" + n2);
                 System.out.print(" = " + (n1 / n2));
                 break;
+
+            default: System.out.println("404");
         }
     }
 }
