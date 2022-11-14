2c938b9d1ecd78a4e7aca8a45242daa65ead6fa4
emirhantash <tasemirhan2546@gmail.com>
2022-11-14 15:08:27 +0300

    java practices
    grade-avg-cal updated and edited
    patika.dev added
    kdv-cal added
    hypo-cal added
    taxi-meter-cal added
    circle-area-perimeter-cal added
    bmi-cal added
    grocery-store added
    simple-cal edited.
    login-menu edited
    air-cond added
    sort-num added
    horoscope-cal edited
    flight-ticket-price added
    chinese-zodiac added
    leap-year added
    number-avg-cal added
    even-multiple4-total-cal added

--- even-multiple4-total-cal/src/Main.java
+++ even-multiple4-total-cal/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int entNum = 0,total = 0;
         Scanner inp = new Scanner(System.in);
-        System.out.println("Please enter a number");
         while (entNum % 2 == 0){
+            System.out.println("Please enter a number");
             entNum = inp.nextInt();
             if(entNum % 4==0)
                 total += entNum;
