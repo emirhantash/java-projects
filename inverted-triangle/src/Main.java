f92ee53095ca67b696f78d1642bf2a940caba59b
emirhantash <tasemirhan2546@gmail.com>
2022-11-16 23:06:42 +0300

    java practices
    grade-avg-cal updated and edited
    patika.dev added
    kdv-cal added
    hypo-cal added
    taxi-meter-cal added
    circle-area-perimeter-cal added
    bmi-cal added
    grocery-store added
    simple-cal edited.
    login-menu edited
    air-cond added
    sort-num added
    horoscope-cal edited
    flight-ticket-price added
    chinese-zodiac added
    leap-year added
    number-avg-cal added
    even-multiple4-total-cal added
    pow-of-4-and-5 edited
    combination-cal added
    exponential-cal edited
    sum-of-digits added
    harmonic-series added
    dia-w-stars added
    atm-project added
    gcd-lcm added
    largest-smallest added
    perfect-number added
    inverted-triangle added

--- inverted-triangle/src/Main.java
+++ inverted-triangle/src/Main.java
@@ -9,22 +9,11 @@ public class Main {
             n = inp.nextInt();
         }while (n < 0);
 
-        //part of top
         for(int i = 1; i <= n; i++) {
-            for(int j = 1 ; j <= (n - i); j++) {
-                System.out.print(" ");
-            }
-            for(int k = 1 ; k <= (2 * i) - 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        //part of bottom
-        for(int i = 2; i <= n; i++) {
             for(int j = 1 ; j <= i - 1 ; j++) {
                 System.out.print(" ");
             }
-            for(int k = (2 * n) - 2; k >= (2*i) - 2 ; k--) {
+            for(int k = (2 * n) - 1; k >= (2*i) - 1 ; k--) {
                 System.out.print("*");
             }
             System.out.println();
---
+++ inverted-triangle/src/README.md
@@ -0,0 +1,20 @@
+# **Inverted Triangle**
+
+*We write a program that takes the number of digits from the user and draws an inverted triangle to the screen with asterisks ( * ) using loops in Java*
+
+### *Example*
+
+*Number of Digits: 10*
+
+```
+ *******************
+  *****************
+   ***************
+    *************
+     ***********
+      *********
+       *******
+        *****
+         ***
+          *
+```
