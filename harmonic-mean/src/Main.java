6fa238216b1f7263b5e9ab3e0a281b9f131a9050
emirhantash <tasemirhan2546@gmail.com>
2022-12-12 23:27:27 +0300

    java practices
    grade-avg-cal updated and edited
    patika.dev added
    kdv-cal added
    hypo-cal added
    taxi-meter-cal added
    circle-area-perimeter-cal added
    bmi-cal added
    grocery-store added
    simple-cal edited.
    login-menu edited
    air-cond added
    sort-num added
    horoscope-cal edited
    flight-ticket-price added
    chinese-zodiac added
    leap-year added
    number-avg-cal added
    even-multiple4-total-cal added
    pow-of-4-and-5 edited
    combination-cal added
    exponential-cal edited
    sum-of-digits added
    harmonic-series added
    dia-w-stars added
    atm-project added
    gcd-lcm added
    largest-smallest added
    perfect-number added
    inverted-triangle added
    prime-number added
    fibonacci added
    palindrome-number added
    recursive-fibonacci added
    advanced-cal added
    recursive-exponent-cal added
    recursive-prime-number added
    recursive-pattern added
    harmonic-mean edited

--- harmonic-mean/src/Main.java
+++ harmonic-mean/src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] array= {1,2,3,4,5,6};
         double add = 0.0, avg;
-
         for(int i : array)
             add += (1.0/i);
-
         System.out.println("Harmonic Series: " + add);
         avg = array.length/add;
         System.out.println("Harmonic Means: " + avg);
